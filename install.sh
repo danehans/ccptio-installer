40004fd02bc5e85227211439da8ab120d8baaee8
Daneyon Hansen <danehans@cisco.com>
2018-07-23 11:15:55 -0700

    Refactors helm param ingestion

--- install.sh
+++ install.sh
@@ -16,9 +16,8 @@ export INSTALL_DIR="${INSTALL_DIR:-$HOME}"
 export BIN_DIR="${BIN_DIR:-/usr/local/bin}"
 export ISTIO_INJECT_NS="${ISTIO_INJECT_NS:-default}"
 export INSTALL_BOOKINFO="${INSTALL_BOOKINFO:-true}"
-export ENABLE_TRACING="${ENABLE_TRACING:-false}"
-export ENABLE_MTLS="${ENABLE_MTLS:-false}"
 export DAILY_BUILD="${DAILY_BUILD:-false}"
+export HELM_PARAMS="${HELM_PARAMS:-}"
 
 # Check for Root user.
 if [ "$(id -u)" != "0" ]; then
@@ -159,24 +158,17 @@ if [ "$(stat ${ISTIO_MANIFEST} 2> /dev/null)" ]; then
     echo "### Kubernetes manifest ${ISTIO_MANIFEST} currently rendered, skipping ..."
     echo "### Run \"rm -rf ${ISTIO_MANIFEST}\" to re-render the Kubernetes manifest ..."
 fi
-if [ "${ENABLE_TRACING}" = "true" ] ; then
-    helm template ${ISTIO_DIR}/install/kubernetes/helm/istio \
-    --set ingressgateway.service.type=NodePort --set tracing.enabled=true --set galley.enabled=true \
-    --name istio --namespace ${ISTIO_NAMESPACE} > ${ISTIO_MANIFEST}
-elif [ "${ENABLE_MTLS}" = "true" ] ; then
-    helm template ${ISTIO_DIR}/install/kubernetes/helm/istio \
-    --set ingressgateway.service.type=NodePort --set global.mtls.enabled=true --set galley.enabled=true \
-    --name istio --namespace ${ISTIO_NAMESPACE} > ${ISTIO_MANIFEST}
+
+ccp_params="--set ingressgateway.service.type=NodePort --set galley.enabled=true"
+
+helm template ${ISTIO_DIR}/install/kubernetes/helm/istio $ccp_params ${HELM_PARAMS} \
+--name istio --namespace ${ISTIO_NAMESPACE} > ${ISTIO_MANIFEST}
+if [ $? -eq 0 ] ; then
+    echo "### Using Helm parameters: \"$ccp_params ${HELM_PARAMS}\" for Istio deployment ..."
+    echo "### Rendered ${ISTIO_MANIFEST} Kubernetes manifest for Istio deployment ..."
 else
-    helm template ${ISTIO_DIR}/install/kubernetes/helm/istio \
-    --set ingressgateway.service.type=NodePort --set galley.enabled=true \
-    --name istio --namespace ${ISTIO_NAMESPACE} > ${ISTIO_MANIFEST}
-    if [ $? -eq 0 ] ; then
-        echo "### Rendered ${ISTIO_MANIFEST} Kubernetes manifest for Istio deployment ..."
-    else
-        echo "### Failed to render ${ISTIO_MANIFEST} Kubernetes manifest for Istio deployment ..."
-        exit 1
-    fi
+    echo "### Failed to render ${ISTIO_MANIFEST} Kubernetes manifest for Istio deployment ..."
+    exit 1
 fi
 
 # Create Kubernetes namespace used for Istio.
